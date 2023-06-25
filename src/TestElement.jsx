f78c210e0ab0a5b62f566d53389cbadc2260b3cd
GehanPasindhu <pasindhu97@gmail.com>
2023-06-25 08:27:42 +0530

    new Update

--- src/TestElement.jsx
+++ src/TestElement.jsx
@@ -1,38 +1,37 @@
-import React, { useState } from 'react';
-import Draggable from 'react-draggable';
+import React, { useEffect, useRef, useState } from "react";
+import Draggable from "react-draggable";
 
 const ElementContainer = () => {
   const [elements, setElements] = useState([]);
+  const [deleteElementId, setDeleteElementId] = useState(null);
+  const [activeElementId, setActiveElementId] = useState(null);
+  const [dropzoneHeight, setDropzoneHeight] = useState(0);
 
   const handleDragStart = (e, buttonId) => {
-    e.dataTransfer.setData('buttonId', buttonId.toString());
-    e.dataTransfer.effectAllowed = 'move';
-  
-    const draggingElement = document.createElement('div');
-    draggingElement.style.width = '150px';
-    draggingElement.style.height = '150px';
-    draggingElement.style.backgroundColor = 'transparent';
-    draggingElement.style.border = '2px dashed yellow';
+    e.dataTransfer.setData("buttonId", buttonId.toString());
+    e.dataTransfer.effectAllowed = "move";
+
+    const draggingElement = document.createElement("div");
+    draggingElement.style.width = "150px";
+    draggingElement.style.height = "150px";
+    draggingElement.style.backgroundColor = "transparent";
+    draggingElement.style.border = "2px dashed yellow";
     draggingElement.innerText = `Element ${buttonId}`;
-  
+
     document.body.appendChild(draggingElement);
-  
+
     // Set the dragged element as the drag image
     e.dataTransfer.setDragImage(draggingElement, 0, 0);
-  
+
     setTimeout(() => {
       document.body.removeChild(draggingElement);
     }, 0);
   };
-  
-  
-  
-  
 
   const handleDragEnd = (e) => {
-    const draggingElement = document.querySelector('.dragging-element');
+    const draggingElement = document.querySelector(".dragging-element");
     if (draggingElement) {
-      draggingElement.classList.remove('dragging-element');
+      draggingElement.classList.remove("dragging-element");
     }
   };
 
@@ -42,26 +41,26 @@ const ElementContainer = () => {
 
   const handleDrop = (e) => {
     e.preventDefault();
-    const buttonId = e.dataTransfer.getData('buttonId');
-    
+    const buttonId = e.dataTransfer.getData("buttonId");
+
     const dropX = e.clientX - e.currentTarget.getBoundingClientRect().left;
     const dropY = e.clientY - e.currentTarget.getBoundingClientRect().top;
-    
-    const elementWidth = 150; 
+
+    const elementWidth = 150;
     const elementHeight = 150;
-    
+
     const dropzoneWidth = e.currentTarget.offsetWidth;
     const dropzoneHeight = e.currentTarget.offsetHeight;
-    
+
     if (
       dropX < 0 ||
       dropX + elementWidth > dropzoneWidth ||
       dropY < 0 ||
       dropY + elementHeight > dropzoneHeight
     ) {
-      return; 
+      return;
     }
-    
+
     const newElement = {
       id: Date.now(),
       name: `Element ${buttonId}`,
@@ -69,66 +68,100 @@ const ElementContainer = () => {
         x: dropX,
         y: dropY,
       },
-      showDeleteButton: true, 
+      showDeleteButton: true,
     };
-  
+
     setElements((prevElements) => [...prevElements, newElement]);
   };
-  
+
   const handleDelete = (elementId) => {
-    setElements((prevElements) =>
-      prevElements.filter((element) => element.id !== elementId)
-    );
+    setDeleteElementId(elementId);
   };
 
+  const handleConfirmationAction = (action) => {
+    if (action === "delete") {
+      const updatedElements = elements.filter((element) => element.id !== deleteElementId);
+      setElements(updatedElements);
+    }
+    setDeleteElementId(null);
+  };
+
+  const dropzoneRef = useRef(null)
+  useEffect(() => {
+    if (dropzoneRef.current) {
+      const { height } = dropzoneRef.current.getBoundingClientRect();
+      setDropzoneHeight(height + 20);
+    }
+  }, [elements]);
+  
+  
   return (
-    <div style={{margin:"10px"}}>
+    <div style={{ margin: "10px" }}>
       <div
         className="dropzone"
+        ref={dropzoneRef}
         onDragOver={handleDragOver}
         onDrop={handleDrop}
-        style={{ height: '85vh', width: '95vw', border:"2px solid red" }}
+        style={{minHeight:"100vh", height:`${dropzoneHeight}px`,
+         width: "95vw", border: "2px solid red", overflow: "auto" }}
       >
         {elements.map((element) => (
           <Draggable
-        grid={[25, 25]}
-        scale={1}
-            >
+          >
           <div
+            onClick={()=>setActiveElementId(element.id)}
             key={element.id}
             id={element.id}
-            className="draggable-element"
+            className={`draggable-element rounded-xl border-1 border-blue-50 p-2 ${activeElementId === element.id ? "z-30" : "z-10"} absolute`}
             style={{
-              position: 'absolute',
               left: element.position.x,
               top: element.position.y,
-              width: '150px',
-              height: '150px',
-              backgroundColor: '#FF0000',
+              width: "400px",
+              height: "400px",
+              backgroundColor: "#FF0000",
             }}
           >
-            {element.name}
-            {element.showDeleteButton && ( 
-              <button
-                className="delete-button"
-                onClick={() => handleDelete(element.id)}
-              >
-                Delete
-              </button>
-            )}
+            <div className="element-content">
+              <div className="flex flex-row justify-between items-baseline gap-4">
+                <div className="flex">
+                  <div className="text-xl">{element.name}</div>
+                </div>
+                <div className="flex">
+                  <div className="flex flex-col gap-2 menu-buttons">
+                    <div className="flex flex-row justify-end gap-1 ">
+                      <div className="w-1 h-1 rounded-full bg-white ring-1"></div>
+                      <div className="w-1 h-1 rounded-full bg-white ring-1"></div>
+                      <div className="w-1 h-1 rounded-full bg-white ring-1"></div>
+                    </div>
+                    <div className="flex flex-row justify-end">
+                      <div className="element-buttons bg-indigo-500 p-2 rounded-lg w-24 z-10">
+                        <button className="move-button cursor-move">
+                          Move
+                        </button>
+                        <button className="edit-button">Edit</button>
+                        <button
+                          className="delete-button"
+                          onClick={() => handleDelete(element.id)}
+                        >
+                          Delete
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           </Draggable>
         ))}
       </div>
-      <div className="button-container">
+      <div className="button-container z-50 fixed bottom-0 gap-2">
         <button
           draggable
           onDragStart={(e) => handleDragStart(e, 1)}
           onDragEnd={handleDragEnd}
           id="btn1"
-          style={{
-            backgroundColor: '#FF0000',
-          }}
+          
         >
           Element 1
         </button>
@@ -137,9 +170,7 @@ const ElementContainer = () => {
           onDragStart={(e) => handleDragStart(e, 2)}
           onDragEnd={handleDragEnd}
           id="btn2"
-          style={{
-            backgroundColor: '#FF0000',
-          }}
+          
         >
           Element 2
         </button>
@@ -148,13 +179,27 @@ const ElementContainer = () => {
           onDragStart={(e) => handleDragStart(e, 3)}
           onDragEnd={handleDragEnd}
           id="btn3"
-          style={{
-            backgroundColor: '#FF0000',
-          }}
+          
         >
           Element 3
         </button>
       </div>
+
+      {deleteElementId && (
+        <div className="confirmation-dialog z-50">
+          <p>Are you sure you want to delete this element?</p>
+          <div className="flex flex-row justify-end items-center gap-2">
+          <button onClick={() => handleConfirmationAction("delete")}>
+            Delete
+          </button>
+          <button onClick={() => handleConfirmationAction("cancel")}>
+            Cancel
+          </button>
+          </div>
+          
+        </div>
+      )}
+
     </div>
   );
 };
